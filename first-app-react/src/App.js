1b004ebfea240ee1f925e28eed7aed1eb9a690a5
idaliaFelsmann <105213955+idaliaFelsmann@users.noreply.github.com>
2022-09-09 18:15:38 +0200

    Initial commit

---
+++ first-app-react/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          this is just a draft, react app through visual code. 
+          First create a folder with the app
+          Afterwards open it w Visual Code  
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
